269aa2ecd07febf90b7f5a6f082ce1b9e0c0f9b9
Hugo <hugo.chaloyard@le-campus-numerique.fr>
2024-02-13 14:05:58 +0100

    Ajout de git got git got dans le fotter.tpl.php

--- resources/views/layouts/footer.tpl.php
+++ resources/views/layouts/footer.tpl.php
@@ -1,3 +1,10 @@
 <?php
 
 echo "test footer" . '<br \>';
+echo "git got git got git got git got git got git got git got git got git got git got git got ". '<br \>';
+echo "got git got git got git got git got git got git got git got git got git got git got git ". '<br \>';
+echo "git got git got git got git got git got git got git got git got git got git got git got ". '<br \>';
+echo "got git got git got git got git got git got git got git got git got git got git got git ". '<br \>';
+echo "git got git got git got git got git got git got git got git got git got git got git got ". '<br \>';
+echo "got git got git got git got git got git got git got git got git got git got git got git ". '<br \>';
+echo "git got git got git got git got git got git got git got git got git got git got git got ". '<br \>';
