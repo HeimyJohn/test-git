d96b24072d44118b9bb39526b0b264c684db936e
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-02-13 14:21:53 +0100

    add button for navbar

--- resources/views/layouts/header.tpl.php
+++ resources/views/layouts/header.tpl.php
@@ -5,13 +5,15 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boutique</title>
     <link rel="stylesheet" href="../css/style.css">
+    <script type="text/javascript" src="../js/script.js"></script>
 </head>
-<header>
+<header class="open">
     <nav>
         <ul>
             <li><a href="/index.php?action=/">Home</a></li>
@@ -21,9 +23,5 @@ session_start();
             <li><a href="/index.php?action=resetCartController">ResetCart</a></li>
         </ul>
     </nav>
+    <button class="header__button"></button>
 </header>
-
-<br>
-<br>
-
-<?php echo "test header" . '<br \>'; ?>
